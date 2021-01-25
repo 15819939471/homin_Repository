0a6c7658b954584eceb08d887f57b1cf07a6f3c0
homin <aaaaaaa>
2021-01-25 19:57:24 +0800

    提交

--- health_parent/health_common/src/main/java/com/itheima/health/constant/MessageConstant.java
+++ health_parent/health_common/src/main/java/com/itheima/health/constant/MessageConstant.java
@@ -4,6 +4,7 @@ package com.itheima.health.constant;
  * 消息常量
  */
 public interface MessageConstant {
+	
     static final String SENDTYPE_ORDER = "001";//用于缓存体检预约时发送的验证码
     static final String SENDTYPE_LOGIN = "002";//用于缓存手机号快速登录时发送的验证码
     static final String SENDTYPE_GETPWD = "003";//用于缓存找回密码时发送的验证码
